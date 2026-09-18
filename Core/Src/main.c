a670467f3cf732ce83a3f69d3a77a206130b39b8
andresb <andresben20@gmail.com>
2026-09-17 17:58:36 -0700

    a

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+int x;
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
